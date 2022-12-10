7daccb1ba76733cc75b5f711060d8a4e0e6522b4
nicklausw <n@nicklausw.com>
2022-12-09 23:35:35 -0500

    custom bot personality

--- src/robbie/core.clj
+++ src/robbie/core.clj
@@ -28,19 +28,46 @@
 ; the bot can remember nicknames
 (def nicknames (atom (:nicknames slurped-memory)))
 
+; the bot has a different personality per guild
+(def personalities (atom (:personalities slurped-memory)))
+
 (defn vec-remove [f e] (vec (remove f e)))
 
 (defn empty-array-if-nil
   [array]
   (if (nil? array) [] array))
 
-(defn get-nickname
+(defn get-personality
+  "note: this returns the whole object.
+   { guild-id, personality, name }"
+  [guild-id]
+  (let [empty-if-nil (fn [name] (if (nil? name) {:guild-id guild-id :personality "" :name "Ditto"} name))]
+    (->> (some #(when (= (:guild-id %) guild-id) %) @personalities)
+         empty-if-nil)))
+
+(defn set-bot-prompt
+  [guild-id prompt]
+  (let [existing (get-personality guild-id)]
+    (reset!
+     personalities 
+     (conj (vec-remove #(= (:guild-id %) guild-id) @personalities) 
+           (assoc existing :personality prompt)))))
+
+(defn set-bot-name
+  [guild-id name]
+  (let [existing (get-personality guild-id)]
+    (reset!
+     personalities
+     (conj (vec-remove #(= (:guild-id %) guild-id) @personalities)
+           (assoc existing :name name)))))
+
+(defn get-user-nickname
   [guild-id user-id] 
   (let [person-if-nil (fn [name] (if (nil? name) "Person" name))]
     (->> (some #(when (and (= (:guild-id %) guild-id) (= (:user-id %) user-id)) (:nickname %)) @nicknames) 
          person-if-nil)))
 
-(defn set-nickname
+(defn set-user-nickname
   [guild-id user-id nickname]
   (reset!
    nicknames
@@ -48,8 +75,8 @@
          {:guild-id guild-id :user-id user-id :nickname nickname})))
 
 (defn append-new-message
-  [old-messages new-message-user new-message-bot nickname]
-  (let [new-list (into old-messages [(str nickname ":" new-message-user) (str "Robbie:" new-message-bot)])]
+  [old-messages new-message-user new-message-bot user-nickname bot-nickname]
+  (let [new-list (into old-messages [(str user-nickname ":" new-message-user) (str bot-nickname ":" new-message-bot)])]
     (if (> (count new-list) 6)
       (-> new-list
           rest
@@ -68,8 +95,9 @@
    old-messages
    new-message-user
    new-message-bot
-   nickname]
-  (let [updated-messages (append-new-message old-messages new-message-user new-message-bot nickname)]
+   user-nickname
+   bot-nickname]
+  (let [updated-messages (append-new-message old-messages new-message-user new-message-bot user-nickname bot-nickname)]
     (reset!
      memory
      (conj (vec-remove #(and (= channel-id (:channel-id %)) (= guild-id (:guild-id %))) @memory) 
@@ -87,11 +115,6 @@
           :channel-id channel-id
           :messages []})))
 
-(def personality
-  (-> (slurp "personality.txt")
-      (str/replace "\r\n" " ")
-      (str/replace "\n" " ")))
-
 (defn messages-to-prompts
   [messages]
   (loop [processed ""
@@ -101,15 +124,15 @@
       (recur (str processed (first unprocessed) "\n") (rest unprocessed)))))
 
 (defn get-openai-response
-  [s messages nickname]
-  (println "sending prompt " (str personality "\n" (messages-to-prompts messages) nickname ":" s "\nRobbie: "))
+  [s messages user-nickname bot-nickname personality]
+  (println "sending prompt " (str personality "\n" (messages-to-prompts messages) user-nickname ":" s "\n" bot-nickname ": "))
   (client/post
    "https://api.openai.com/v1/completions"
    {:headers {"Authorization" (str "Bearer " (System/getenv "OPENAI_TOKEN"))}
     :content-type :json
     :form-params
     {:model "text-davinci-003" 
-     :prompt (str personality "\n" (messages-to-prompts messages) nickname ":" s "\nRobbie: ") 
+     :prompt (str personality "\n" (messages-to-prompts messages) user-nickname ":" s "\n" bot-nickname ": ") 
      :temperature 0 
      :max_tokens 1000}}))
 
@@ -117,9 +140,9 @@
   "[The OpenAI server sent back an error. Please try again in a minute; the server may be busy.]")
 
 (defn get-response
-  [s messages nickname]
+  [s messages user-nickname bot-nickname personality]
   (try
-    (let [response (get-openai-response s messages nickname)]
+    (let [response (get-openai-response s messages user-nickname bot-nickname personality)]
       (if (= (:status response) 200)
         (let [body (cheshire/parse-string (:body response) true)]
           (trim-newlines (:text (first (:choices body)))))
@@ -130,32 +153,51 @@
 
 (defmethod handle-event :message-create
   [_ {:keys [guild-id channel-id author content] :as _data}] 
+  (println "got input: " content)
   (cond
     (str/starts-with? content "/gen")
     (let [trimmed-content (trim-left content "/gen")
-          nickname (get-nickname guild-id (:id author))]
+          personality-obj (get-personality guild-id)
+          bot-nickname (:name personality-obj)
+          personality (:personality personality-obj)
+          user-nickname (get-user-nickname guild-id (:id author))]
       (println "generating response for message...")
       (discord-rest/trigger-typing-indicator! (:rest @state) channel-id)
       (let [messages (get-messages guild-id channel-id)
-            bot-content (get-response trimmed-content messages nickname)]
+            bot-content (get-response trimmed-content messages user-nickname bot-nickname personality)]
         (discord-rest/create-message! (:rest @state) channel-id
                                       :content (str (mention-user author) " " bot-content))
-        (update-message-history guild-id channel-id messages trimmed-content bot-content nickname)
+        (update-message-history guild-id channel-id messages trimmed-content bot-content user-nickname bot-nickname)
         (reset! changed true)
         (println "done.")))
-    
+
     (str/starts-with? content "/nickname ")
     (let [trimmed-content (trim-left content "/nickname ")]
       (println "setting nickname to" trimmed-content "...")
-      (set-nickname guild-id (:id author) trimmed-content)
+      (set-user-nickname guild-id (:id author) trimmed-content)
       (discord-rest/create-message! (:rest @state) channel-id
                                     :content (str (mention-user author) " Set name to " trimmed-content ".")))
-    
+
+    (str/starts-with? content "/botname ")
+    (let [trimmed-content (trim-left content "/botname ")]
+      (println "setting bot nickname to" trimmed-content "...")
+      (set-bot-name guild-id trimmed-content)
+      (discord-rest/modify-current-user-nick! (:rest @state) guild-id trimmed-content)
+      (discord-rest/create-message! (:rest @state) channel-id
+                                    :content (str (mention-user author) " My name is now " trimmed-content ".")))
+
+    (str/starts-with? content "/personality ")
+    (let [trimmed-content (trim-left content "/personality ")]
+      (println "setting bot personality to" trimmed-content "...")
+      (set-bot-prompt guild-id trimmed-content)
+      (discord-rest/create-message! (:rest @state) channel-id
+                                    :content (str (mention-user author) " My personality prompt is now " trimmed-content ".")))
+
     (= content "/reset")
-      (do 
-        (delete-message-history guild-id channel-id) 
-        (println "message history reset for channel " channel-id)
-        (reset! changed true))))
+    (do
+      (delete-message-history guild-id channel-id)
+      (println "message history reset for channel " channel-id)
+      (reset! changed true))))
 
 (defmethod handle-event :default [_ _])
 
@@ -182,12 +224,14 @@
   [time] 
   (when (true? @changed)
     (println (.toString time) ": Writing to memory.json")
-    (spit "memory.json" (cheshire/generate-string {:memory @memory :nicknames @nicknames} {:pretty true}))
+    (spit "memory.json" (cheshire/generate-string {:memory @memory
+                                                   :nicknames @nicknames
+                                                   :personalities @personalities} {:pretty true}))
     (reset! changed false))
   (reset-timer))
 
 (defn -main [& args]
-  (reset! state (start-bot! (System/getenv "ROBBIE_TOKEN") :guild-messages))
+  (reset! state (start-bot! (System/getenv "DITTO_TOKEN") :guild-messages))
   (reset! bot-id (:id @(discord-rest/get-current-user! (:rest @state))))
   (reset-timer)
   (try
