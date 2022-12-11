f63a08052e35e01ea9fbfcaa043e9920439ae156
nicklausw <n@nicklausw.com>
2022-12-11 15:14:19 -0500

    update data about project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Ditto
+Discord bot that lets you make any personality you'd like out of GPT-3.
+
+## Tokens Used
+* `DITTO_TOKEN` - Discord bot token.
+* `OPENAI_TOKEN` - OpenAI token.
+
+## Commands
+* `/gen [response]` - get a response from the bot via OpenAI.
+* `/nickname [name]` - set your nickname for the bot to call you.
+* `/botname [name]` - set the bot's nickname.
+* `/personality [content]` - Input a small bit of text speaking in third-person about the bot's personality. Refer to them by name.
+* `/reset` - Reset the bot's memory of past messages from the current channel.
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject ditto "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject ditto "beta"
+  :description "GPT-3 Discord bot with any personality you like."
+  :url "http://github.com/nicklausw/ditto"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.suskalo/discljord "1.1.1"]
                  [clj-http "3.12.3"]
